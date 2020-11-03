5135e873fde7463352c0171598fb1309cb1256ef
ansary14 <ayubuapcse@gmail.com>
2020-11-03 18:22:46 +0600

    Modify the input box, functionality and create custom scope for message view

--- app/controllers/chatroom_controller.rb
+++ app/controllers/chatroom_controller.rb
@@ -1,8 +1,9 @@
 class ChatroomController < ApplicationController
   before_action :require_user
-  
+
   def index
-    @messages = Message.all
+    @message =  Message.new
+    @messages = Message.custom_display
   end
 
 end
--- app/models/message.rb
+++ app/models/message.rb
@@ -1,6 +1,7 @@
 class Message < ApplicationRecord
 
   belongs_to :user
-  validates :body, presence: true 
+  validates :body, presence: true
+  scope :custom_display, -> { order(:created_at).last(20) }
 
 end
